de27a8fa29da0d2a820a961863f0c22d5cadb18c
Nikolai Kiselev <10960944+nikkiselev@users.noreply.github.com>
2019-04-08 13:31:39 +0400

    Add solution #9

---
+++ solutions/9.js
@@ -0,0 +1,21 @@
+// https://projecteuler.net/problem=8
+
+/**
+ * Pythagorean triplet for which a + b + c equals to the given sum.
+ *
+ * @param {number} sum
+ * @returns {number}
+ */
+const productOfPythagoreanTriplet = (sum) => {
+  for (let a = 3; a < sum; a += 1) {
+    for (let b = 4; a + b < sum; b += 1) {
+      const c = sum - a - b;
+      if (a * a + b * b === c * c) {
+        return a * b * c;
+      }
+    }
+  }
+  return undefined;
+};
+
+console.log(productOfPythagoreanTriplet(1000));
