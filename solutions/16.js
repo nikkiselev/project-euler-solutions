d907d1266f959242c52910536a8c3ec057e9de31
Nikolai Kiselev <10960944+nikkiselev@users.noreply.github.com>
2019-06-20 20:29:43 +0400

    Convert to arrow function

--- solutions/16.js
+++ solutions/16.js
@@ -1,10 +1,10 @@
 // https://projecteuler.net/problem=16
 
-function powerDigitSum(num) {
-  return BigInt(2 ** num)
+const powerDigitSum = num => (
+  BigInt(2 ** num)
     .toString(10)
     .split('')
-    .reduce((a, b) => +a + +b);
-}
+    .reduce((a, b) => +a + +b)
+);
 
 console.log(powerDigitSum(1000));
