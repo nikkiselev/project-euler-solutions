29f919f85b9474011601ea1992381ebc12c7884e
Nikolai Kiselev <10960944+nikkiselev@users.noreply.github.com>
2019-07-02 20:28:51 +0700

    Add solution #19

---
+++ solutions/19.js
@@ -0,0 +1,15 @@
+// https://projecteuler.net/problem=19
+
+let date = new Date('1901-01-01');
+
+let numberOfSundays = 0;
+
+while (date.getFullYear() < 2001) {
+  date = new Date(Date.UTC(date.getFullYear(), date.getMonth() + 1, date.getDate()));
+  
+  if (date.getDay() === 0) {
+    numberOfSundays += 1;
+  }
+}
+
+console.log(numberOfSundays);
